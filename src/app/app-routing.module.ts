7e205bdd4123a20523f165a77ad169ae24b80523
louay elaroui <you@example.com>
2023-10-03 21:32:02 +0200

    new

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+   path: 'visiteur', loadChildren: () => import('./visiteur/visiteur.module').then(m => m.VisiteurModule) }, 
+   { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes,{
+    preloadingStrategy: PreloadAllModules
+    } )],  exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/visiteur/home-visiteur/home-visiteur.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-visiteur',
+  templateUrl: './home-visiteur.component.html',
+  styleUrls: ['./home-visiteur.component.css']
+})
+export class HomeVisiteurComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
