1a86079e2da798936cf898366dd5bcff7cc66de0
louay elaroui <you@example.com>
2023-10-10 20:35:36 +0200

    liste des films

--- src/app/visiteur/home-visiteur/home-visiteur.component.ts
+++ src/app/visiteur/home-visiteur/home-visiteur.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FILMS } from 'src/app/shared/models/des-films';
+import { Film } from 'src/app/shared/models/film';
 
 @Component({
   selector: 'app-home-visiteur',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-visiteur.component.css']
 })
 export class HomeVisiteurComponent implements OnInit {
-
+  tabFilms: Film[] = FILMS;
   constructor() { }
 
   ngOnInit(): void {
+  }
+  affiche(f:Film){
+    f.descVisible=true;
+  }
+  cacher(f:Film){
+    f.descVisible=false;
   }
 
 }
